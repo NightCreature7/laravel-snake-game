44be9636839006d6314e22c574ae8594f843c0c4
NightCreature7 <81756425+NightCreature7@users.noreply.github.com>
2023-11-13 13:52:06 +0100

    Small correction in the routes

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [GameController::class, 'showGame']);
 Route::post('/store-score', [GameController::class, 'storeScore'])->name('storeScore');
 Route::get('/leaderboard', [GameController::class, 'showLeaderboard'])->name('leaderboard');
-Route::get('/leaderboard', [GameController::class, 'showLeaderboard'])->name('leaderboard');
